6e186dc73faacdad2656e55fb6d5baa81447798b
Adrien <adrien.gvn@eduge.ch>
2022-09-29 15:39:06 +0200

    Page d'acceuil, Page de login, Création d'équipe (Formulaire + Page), Backlog pour Admin

--- pages/equipe.php
+++ pages/equipe.php
@@ -10,9 +10,6 @@ $nom_equipe = filter_input(INPUT_POST, "nom_equipe", FILTER_UNSAFE_RAW);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,600;1,200&display=swap" rel="stylesheet">
 </head>
 
 <body class="equipe">
@@ -57,9 +54,9 @@ $nom_equipe = filter_input(INPUT_POST, "nom_equipe", FILTER_UNSAFE_RAW);
     <header>
         <h1>KDAM</h1>
         <ul>
-            <li><a href="../index.html">Homepage</a></li>
-            <li><a href="pages/match.html">Match</a></li>
-            <li><a href="pages/equipe.html">Equipe</a></li>
+            <li><a href="../index.php">Homepage</a></li>
+            <li><a href="">Match</a></li>
+            <li><a href="">Equipe</a></li>
             <li><a href="">Joueurs</a></li>
         </ul>
         <form action="Connection/connection.php" method="post">
@@ -81,74 +78,47 @@ $nom_equipe = filter_input(INPUT_POST, "nom_equipe", FILTER_UNSAFE_RAW);
         <h2 class="equipe">Equipe</h2>
 
         <article class="equipe">
-            <div class="equipe">
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
-            </div>
+        <div class="equipe">
+            <p class="nom_equipe">Suisse</p>
+            <p class="coach">Murat Yakin</p>   
+            <p class="date_creation">1955</p>
+        </div>
+        <?php
+            $servername = 'localhost';
+            $username = 'adriengvn';
+            $password = 'root';
+            
+            //On établit la connexion
+            $conn = new PDO("mysql:host=$servername;dbname=Jeudi_Database", $username, $password);
+                        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-            <div class="equipe">
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
-            </div>
+            $sqlQuery = 'SELECT nom_equipe, NomEntraineur, dateX FROM Equipe';
+            $equipeStatement = $conn->prepare($sqlQuery);
+            $equipeStatement->execute();
+            $equipe = $equipeStatement->fetchAll();
 
+            // On affiche chaque recette une à une
+            foreach ($equipe as $equipe) {
+            ?>
             <div class="equipe">
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
+            <p class="nom_equipe"><?php echo $equipe['nom_equipe'];?></p>
+            <p class="coach"><?php echo $equipe['NomEntraineur'];?></p>   
+            <p class="date_creation"><?php echo $equipe['dateX'];?></p>
             </div>
+            <?php
+            }
+    ?>
 
-            <div class="equipe">
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
-            </div>
-
-            <div class="equipe">    
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
-            </div>
-
-            <div class="equipe">
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
-            </div>
-
-            <div class="equipe">
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
-            </div>
-
-            <div class="equipe">
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
-            </div>
-
-            <div class="equipe">
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
-            </div>
-            <div class="equipe">
-                <p class="nom_equipe">Suisse</p>
-                <p class="coach">Vladimir Petcovich</p>
-                <p class="date_creation">1955</p>
-            </div>
         </article>
         <article class="creation">
             <button class="btnCreer">
-                <a class="FakeCreation" href="formEquipe.html">Ajouter une équipe</a>
+                <a class="FakeCreation" href="formEquipe.php">Ajouter une équipe</a>
             </button>
-
+           
         </article>
-
+        
     </main>
-
+   
 </body>
 
 </html>
